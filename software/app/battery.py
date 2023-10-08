df0ad89591ed2cc42072606802718ba1d8aaa890
MakerM0 <jiangmin12youxiang@163.com>
2023-10-08 17:55:03 +0800

    Update battery.py

--- software/app/battery.py
+++ software/app/battery.py
@@ -1,4 +1,8 @@
 '''
+v0.1.1
+    20231008
+    add 'quit action'
+
 
 v0.1.0
     20231008
@@ -6,15 +10,12 @@ v0.1.0
 '''
 
 from magiclick import *
-
-
 import time
 import board
 import analogio
 
 
 display.show(None)
-
 
 TERMINAL_HEIGHT=display.height+20
 display.root_group.scale = 1
@@ -25,16 +26,26 @@ display.root_group[2].hidden = True # status bar
 supervisor.reset_terminal(display.width,TERMINAL_HEIGHT)
 display.root_group[0].y = 0
 
-
 display.show(display.root_group)
 
 analog_pin = analogio.AnalogIn(board.BAT)
 
 def get_voltage(pin):
     return (pin.value * 3.3) / 65535 *2
+
 get_voltage(analog_pin)
 time.sleep(0.5)
 
+now = 0
+old = 0
+
 while True:
-    print(f'{get_voltage(analog_pin)} V')
-    time.sleep(2)
+    now =  time.monotonic()
+    if (now-old) >= 2.0:
+        print(f'{get_voltage(analog_pin)} V')         
+
+    key = getkey() 
+    if key==2 or key==1:
+        print('exit')
+        returnMainPage() 
+    time.sleep(0.2)
